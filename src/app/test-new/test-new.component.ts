24bb5a00e73a413fc415a5004f2db1f15e616380
SzczepanPrzebieda <szczepanprzebieda@gmail.com>
2016-11-02 15:17:35 +0000

    first

---
+++ src/app/test-new/test-new.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-new',
+  templateUrl: './test-new.component.html',
+  styleUrls: ['./test-new.component.css']
+})
+export class TestNewComponent implements OnInit {
+msg: string;
+
+
+  constructor() { 
+    this.msg = "New Component";
+  }
+
+
+  ngOnInit() {
+  }
+
+}
